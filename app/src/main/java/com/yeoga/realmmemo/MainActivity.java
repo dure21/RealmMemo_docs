e3f11fb9e6f35848b14f695d2fd78b40bef1f03d
KJH <jinhungkim@hanmail.net>
2018-11-10 19:48:59 +0900

    NEW

--- app/src/main/java/com/yeoga/realmmemo/MainActivity.java
+++ app/src/main/java/com/yeoga/realmmemo/MainActivity.java
@@ -33,8 +33,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
         setContentView(R.layout.activity_main);
 
 
